4f7d49afb762a779f44b40ca4f61134b8d1d9f09
elentaryy <elentaryy@gmail.com>
2023-07-02 10:24:58 +0300

    fix model prediction

--- backend/app/utils.py
+++ backend/app/utils.py
@@ -20,11 +20,11 @@ def format_data(data):
         
 
         all_hero_ids = set(range(1, 141))
-        for hero_id in all_hero_ids:
-            for team_number in range(2):
-                column_name = f'hero_{hero_id}_team_{team_number}'
-                df[column_name] = df['match_data'].apply(lambda x: 1 if any(player.get('hero_id') == hero_id \
-                                        and player.get('team') == team_number for player in x.get('players', {})) else 0)
+        for team_number in range(2):
+            for hero_id in all_hero_ids:
+                    column_name = f'hero_{hero_id}_team_{team_number}'
+                    df[column_name] = df['match_data'].apply(lambda x: 1 if any(player.get('hero_id') == hero_id \
+                                            and player.get('team') == team_number for player in x.get('players', {})) else 0)
                 
         team1_cols = [f'hero_{hero_id}_team_0' for hero_id in all_hero_ids]
         team2_cols = [f'hero_{hero_id}_team_1' for hero_id in all_hero_ids]
